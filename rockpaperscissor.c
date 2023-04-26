b70facb5403aba101f335caecaa0a763d000dab6
henrik.wirblad@gmail.com <henrik.wirblad@gmail.com>
2023-04-26 14:18:38 +0200

    addad save to file

--- rockpaperscissor.c
+++ rockpaperscissor.c
@@ -2,6 +2,7 @@
 #include <time.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include "safeinput.h"
 #include "rockpaperscissor.h"
 
@@ -27,7 +28,7 @@ void play(char *userChoice, char *computerChoice, int *userWins, int *computerWi
         if(!strcmp(computerChoice,"Paper")){
             printf("IT'S A DRAW\n");   
         }
-        else if(!strcmp(computerChoice,"Scisoot")){
+        else if(!strcmp(computerChoice,"Scissor")){
             printf("YOU LOOSE\n");
             *computerWins = *computerWins +1;   
         }
@@ -72,11 +73,84 @@ bool checkUserInput(char *userChoice){
             return false; 
 }
 
+void saveToFile(char *result, char *date, float averageWins){
+    
+    FILE *fOpen = fopen("stats.txt", "w");
+    if(fOpen == NULL){
+        printf("FEL!!! - Filen kunde inte öppnas");
+        fclose(fOpen);
+        return;
+    }
+
+    fputs(result,fOpen);
+    fprintf(fOpen, "\n"); 
+    fputs(date,fOpen);
+    fprintf(fOpen, "\n"); 
+    fprintf(fOpen, "%f", averageWins); 
+    fprintf(fOpen, "\n"); 
+    fclose(fOpen);
+
+}
+
+void showStats(int userWins, int computerWins){
+
+    time_t rawtime;
+    struct tm *info;
+    char buffer[50];
+
+    time( &rawtime );
+    info = localtime( &rawtime );
+    strftime(buffer,80,"%x - %I:%M%p", info);
+    printf("You played at : %s\n", buffer);
+    float averageWins = ((double)userWins/(double)(userWins+computerWins) * 100); 
+    if(isnan(averageWins))
+        averageWins=0;
+
+    if(userWins > computerWins){
+        printf("YOU WON THE GAME\n");
+        saveToFile("WON", buffer, averageWins);
+    }
+    else if(userWins < computerWins){
+        printf("SORRY YOU LOST THE GAME\n");
+        saveToFile("LOST",buffer, averageWins);
+    }
+    else{
+        printf("ITS A DRAW\n");
+        saveToFile("PLAYED DRAW",buffer,averageWins);
+    }
+    printf("You won %.2f %% of the times\n", averageWins);
+}
+
+void loadfile(){
+
+    FILE *fOpen = fopen("stats.txt", "r");
+
+    if(fOpen == NULL){
+        printf("FEL!!! - Filen kunde inte öppnas");
+        fclose(fOpen);
+        return;
+    }
+
+    char result[250] = "";
+    char date[250] = "";
+    char average[250] = "";
+
+    fgets(result,250,fOpen); 
+    fgets(date,250,fOpen); 
+    fgets(average,250,fOpen);
+
+    printf("LAST TIME YOU PLAYED WAS: %sAND YOU %sYOUR WINRATE WAS: %s\n",date,result,average);
+    
+    fclose(fOpen); 
+
+}
+
 void rockpaperscissor(){
     
-    srand(time(0));
+    loadfile();
+    srand(time(NULL));
     char computerChoice[100] = "";
-    char continuePlaying;
+    char continuePlaying = '\0';
     int userWins = 0;
     int computerWins = 0;
     while(true){
@@ -94,5 +168,6 @@ void rockpaperscissor(){
         if(continuePlaying == 'N' || continuePlaying == 'n')
             break; 
     }
+    showStats(userWins, computerWins);
 }
 
